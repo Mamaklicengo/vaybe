5b6286c4a4a4238c4308b603c18518750875e274
Halil Barak <hbarak36@gmail.com>
2023-09-17 09:36:24 +0300

    design

--- config.py
+++ config.py
@@ -1,21 +1,22 @@
 import json
+from pyrogram import filters
+
 with open("config.json", "r") as f:
     config = json.load(f)
 
 BOT_TOKEN = config.get("BOT_TOKEN", None)
 api_hash= config.get("api_hash", None)
 api_id = config.get("api_id", None)
-from pyrogram import filters
+bot_owner = config.get("bot_owner", None)
+log_grup = config.get("log_grup", None)
+admin_grup = config.get("admin_grup", None)
+admins = config.get("admins", None)
+
 diyanet = "https://kuran.diyanet.gov.tr"
 kuran_sure = 'https://kuran.diyanet.gov.tr/Tefsir/'
 kuran_api = 'https://api.acikkuran.com/authors'
 ezan_vakitleri = "https://namazvakitleri.diyanet.gov.tr/tr-TR"
 
-
-bot_owner = config.get("bot_owner", None)
-log_grup = config.get("log_grup", None)
-admin_grup = config.get("admin_grup", None)
-admins = config.get("admins", None)
 admin_filter = filters.create(lambda _, __, message: message.from_user is not None and message.from_user.id in admins)
 
 ezanlar = ['CQACAgIAAxkBAAEBIUxkLOa9-Uz5P6LL4xu_W9sxkR3N4gACPA0AAjbvQUjejXzzVP3rKB4E', 'CQACAgIAAxkBAAEBIU5kLObVa0o2Qy3c0DkQF9ZLR19W-AAC6goAAu73WEiHdIjXcHtl7B4E',
